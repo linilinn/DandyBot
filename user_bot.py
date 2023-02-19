525600c315b112577fcf6221a34988438152a202
meow <meowmeow@gmail.com>
2023-02-20 01:28:25 +0300

    fixed first level and second level

--- user_bot.py
+++ user_bot.py
@@ -1,9 +1,19 @@
 ﻿def script(check, x, y):
     if check("level") == 1:
-        if check("gold", x, y):
+        if check("gold", x, y) != 0:
             return "take"
         if check("wall", x + 2, y):
             return "down"
         else:
             return "right"
+    elif check("level") == 2:
+        if check("gold", x, y) != 0:
+            return "take"
+        if check("wall", x + 2, y) is False and check("wall", x, y + 1):
+            return "right"
+        if check("gold", x, y + 1) != 0:
+            return "down"
+        if check("wall", x, y - 1) is False:
+            return "up"
+        return "right"
     return "pass"
