0aa8f43182c72dfe4bf37bb7f5659505e5c90df2
meow <meowmeow@gmail.com>
2023-02-20 00:42:44 +0300

    first level

--- user_bot.py
+++ user_bot.py
@@ -1,4 +1,9 @@
 ﻿def script(check, x, y):
     if check("level") == 1:
-        return "right"
+        if check("gold", x, y):
+            return "take"
+        if check("wall", x + 2, y):
+            return "down"
+        else:
+            return "right"
     return "pass"
